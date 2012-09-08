6087ca9d26310fd2eba70704329b760d11608535
Jesse Merritt <jes1510@gmail.com>
2012-09-08 03:05:57 -0300

    Update emailGPIO.py

--- emailGPIO.py
+++ emailGPIO.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 '''
-Written by Jesse Merritt www.github.com/jes1510 September 8 , 2012
+Written by Jesse Merritt 
+www.github.com/jes1510 
+September 8 , 2012
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
